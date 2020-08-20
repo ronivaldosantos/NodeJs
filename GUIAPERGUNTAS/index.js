b40650876a6993c996368bb3b25e39fdb037c319
Ronivaldo Santos <ronivaldosan@gmail.com>
2020-08-20 10:43:59 -0300

    44/45. Obtendo registros do BD e listando no Front-End.

--- GUIAPERGUNTAS/index.js
+++ GUIAPERGUNTAS/index.js
@@ -2,7 +2,8 @@ const express = require("express"); // Importando módulo express
 const app = express(); // atribuindo express a uma variável
 const bodyParser = require("body-parser"); //Recebe os dados enviados pelo FORM
 const connection = require('./database/database'); // importando a conecção com o BD.
-const pergunta = require("./database/Pergunta"); // Importando a criação da tabela.
+const Pergunta = require("./database/Pergunta"); // Importando a criação da tabela.
+const pergunta = require("./database/Pergunta");
 
 //Testando conexão com o banco de dados
 connection
@@ -13,7 +14,6 @@ connection
     .catch((msgErro)=>{
         console.log(msgErro);
     })
-
 
 //Informando ao express para utilizar o EJS como View engine, para renderizar html, ou seja desenhar páginas html.
 app.set('view engine','ejs');
@@ -27,10 +27,17 @@ app.use(bodyParser.json());
 
 //ROTAS
 // IMPORTANTE: Todo arquivo html ou seja EJS deve estar obrigatóriamente dentro da pasta view.
-app.get("/",(req,res)=>{ //Rota inicial do sistema
+app.get("/",(req,res)=>{ //Rota principal inicial  do sistema
+ 
+    //Obtem todas as perguntas cadastradas no BD.
+    // O JSON raw:true informa que trará somente os registros da tabela.
+    Pergunta.findAll({raw: true}).then(perguntas =>{
 
-    // chama arquivo .ejs e renderiza na tela para o usuário. render vai direto na pasta views, para express
-    res.render("index",{
+        // chama arquivo .ejs e renderiza na tela para o usuário. render vai direto na pasta views, para express
+        res.render("index",{
+            perguntas:perguntas // Envia array com registros do BD para a página Index.ejs
+        });    
+
     });
 });
 
@@ -47,7 +54,7 @@ app.post("/salvarpergunta",(req,res)=>{
     var descricao = req.body.descricao;
 
     //Salvando dados do formulário no BD equivalente ao Insert Into
-    pergunta.create({
+    Pergunta.create({
         titulo: titulo,
         descricao: descricao
     // Após a inserção dos dados redireciona para a página principal
