2962b3e7066daea288a30a7719315a563ce3c90a
Ronivaldo Santos <ronivaldosan@gmail.com>
2020-08-22 20:03:03 -0300

    Cadastro de categorias usando Slugify

--- GUIAPRESS/categories/CategoriesController.js
+++ GUIAPRESS/categories/CategoriesController.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
 
 // Rota para criar nova categoria
 router.get("/admin/categories/new",(req,res)=>{
     res.render("admin/categories/new")
 })
+
+// Rota para salavar nova categoria no BD.
+router.post("/categories/save",(req,res)=>{
+    var title = req.body.title;
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title) //Transforma uma string para ex:  De: "Isso e um teste" Para: "isso-e-um-teste"
+        }).then(() =>{
+            res.redirect("/");
+        });
+
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+});
 
 module.exports = router;
